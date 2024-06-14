ecdfeb1f68fbf6a8f8ccc5112e43d5b32a8e5994
Launyx <lunyx.castelhano@gmail.com>
2024-06-14 14:12:14 -0300

    Project created

---
+++ src/main/java/com/Launyx/Project/ProjectApplication.java
@@ -0,0 +1,16 @@
+package com.Launyx.Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// Annotation para configurar o projeto, para que ele seja uma aplicação SpringBoot '@'
+@SpringBootApplication
+public class ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectApplication.class, args);
+
+
+
+	}
+}

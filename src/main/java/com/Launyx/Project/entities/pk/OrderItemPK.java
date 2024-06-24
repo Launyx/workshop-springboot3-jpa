1e8ac44f46ed44b1b5eaa65731d8b42dd1358978
Launyx <lunyx.castelhano@gmail.com>
2024-06-24 19:28:26 -0300

    OrderItem, many-to-many association with extra attributes PART 1

---
+++ src/main/java/com/Launyx/Project/entities/pk/OrderItemPK.java
@@ -0,0 +1,53 @@
+package com.Launyx.Project.entities.pk;
+
+import com.Launyx.Project.entities.Order;
+import com.Launyx.Project.entities.Product;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+// Classe para servir como chave primária composta por duas colunas (Product e Order)
+@Embeddable
+public class OrderItemPK implements Serializable {
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderItemPK that = (OrderItemPK) o;
+        return Objects.equals(order, that.order) && Objects.equals(product, that.product);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(order, product);
+    }
+}
